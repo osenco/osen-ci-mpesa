f6fcdc67b6bbe327dcd83c264d0b562f14310723
Mauko Maunde <maukoese@gmail.com>
2019-08-09 16:11:08 +0300

    Update Pesa.php

--- application/controllers/Pesa.php
+++ application/controllers/Pesa.php
@@ -1,13 +1,14 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class pesa extends CI_Controller {
+class Pesa extends CI_Controller 
+{
+	public $table = 'mpesa';
     
     public function __construct()
     {
         parent::__construct();
 		$this->load->database();
-        $this->load->model('payments_model');
         $this->load->helper('form');
         $this->load->helper('url');
         $this->load->library(
@@ -19,7 +20,6 @@ class pesa extends CI_Controller {
 				'headoffice'        => '174379',
 				'key'               => 'WiGveilGB2SKbXWi9IShIHDK7XfCtvWK',
 				'secret'            => 'mJBnR94sTlGFUkvM',
-				'username'          => 'apitest',	
 				'passkey'           => 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919',
 				'validation_url'    => site_url('pesa/validate'),
 				'confirmation_url'  => site_url('pesa/confirm'),
@@ -27,22 +27,53 @@ class pesa extends CI_Controller {
 				'timeout_url'      	=> site_url('pesa/timeout'),
 			)
 		);
+
+		header('Access-Control-Allow-Origin: *');
     }
 
 	public function index()
 	{
-		$this->load->view('mpesa');
+		$phone 		= isset($_POST['phone']) ? $_POST['phone'] : 2547;
+		$amount 	= isset($_POST['amount']) ? $_POST['amount'] : 10;
+		$reference 	= isset($_POST['reference']) ? $_POST['reference'] : $phone;
+		$data 		= ['config' => $this->mpesa->config, 'reference' => $reference, 'amount' => $amount, 'phone' => $phone];
+		$this->load->view('mpesa', $data);
 	}
     
 	public function pay()
 	{
-        header('Content-type:Application/json');
 		if (isset($_POST['amount'])) {
-			$phone 		= trim($_POST['phone']); 
-			$amount 	= trim($_POST['amount']); 
-			$reference 	= trim($_POST['reference']);
+        	$phone 		= strip_tags($_POST['phone']); 
+			$amount 	= strip_tags($_POST['amount']); 
+			$reference 	= strip_tags($_POST['reference']);
 
-			echo json_encode($this->mpesa->stk($phone, $amount, $reference));
+			$response 	= $this->mpesa->request($phone, $amount, $reference);
+			if($response){
+				if (!isset($response['errorCode'])) {
+					$request = isset($response['MerchantRequestID']) ? $response['MerchantRequestID'] : time();
+					$payment = array(
+						'phone' 	=> $phone,
+						'amount' 	=> $amount,
+						'reference' => $reference,
+						'request'	=> $request,
+						'status'	=> 'pending',
+						'receipt'	=> 'N/A'
+					);
+					$this->db->insert($this->table, $payment);
+				}
+			} else {
+				$response = array(
+					'errorCode' => 1, 
+					'errorMessage' => 'Could not connect to Daraja'
+				);
+			}
+
+			# You can do something with $response, or just redirect to payments page
+			# header('Location: '.site_url('payments'));
+			# exit();
+			# or output the response in JSON, to be processed by an AJAX request
+			# header('Content-type:Application/json');
+			# echo json_encode($response);
 		}
 	}
     
@@ -50,17 +81,39 @@ class pesa extends CI_Controller {
 	{
         header('Content-type: Application/json');
 		header('Access-Control-Allow-Origin: *');
-		echo json_encode($this->mpesa->reconcile(function ($response)
+		$result = $this->mpesa->reconcile(function ($response)
 		{
-			return true;
-		}));
+			$response                   = $response['Body'];
+			$resultCode 			    = $response['stkCallback']['ResultCode'];
+			$resultDesc 			    = $response['stkCallback']['ResultDesc'];
+			$merchantRequestID 			= $response['stkCallback']['MerchantRequestID'];
+
+			if(isset($response['stkCallback']['CallbackMetadata'])){
+				$CallbackMetadata       = $response['stkCallback']['CallbackMetadata']['Item'];
+
+				$amount                 = $CallbackMetadata[0]['Value'];
+				$mpesaReceiptNumber     = $CallbackMetadata[1]['Value'];
+				$phone                  = $CallbackMetadata[4]['Value'];
+			}
+
+			$payment 			= $this->db->query("SELECT FROM {$this->table} WHERE request='{$response['MerchantRequestID']}';")->row();
+			$payment->phone    	= $phone;
+			$payment->amount   	= round($amount);
+			$payment->receipt   = $mpesaReceiptNumber;
+			$payment->status 	= 'paid';
+
+			$this->db->update($this->table, $payment, array('id' => $payment->id));
+
+            return true;
+		});
+
+		echo json_encode($result);
 	}
     
 	public function confirm()
 	{
         header('Content-type: Application/json');
-		header('Access-Control-Allow-Origin: *');
-		echo json_encode($this->mpesa->confirm(function ($response)
+		$result = $this->mpesa->confirm(function ($response)
 		{
             // Process $response
             $TransactionType    = $response['TransactionType'];
@@ -77,14 +130,19 @@ class pesa extends CI_Controller {
             $MiddleName         = $response['MiddleName'];
             $LastName           = $response['LastName'];
 
-            $payment 						= $this->payments_model->get($BillRefNumber);
-            $amount 						= round($TransAmount);
-			$this->payments_model->phone    = $MSISDN;
-			$this->payments_model->amount   = $amount;
-			$this->payments_model->update_entry();
+            $payment 			= $this->db->query("SELECT FROM {$this->table} WHERE reference='{$BillRefNumber}';")->row();
+            $amount 			= round($TransAmount);
+			$payment->phone    	= $MSISDN;
+			$payment->amount   	= $amount;
+			$payment->receipt   = $TransID;
+			$payment->status 	= 'paid';
+
+			$this->db->update($this->table, $payment, array('id' => $payment->id));
 
             return true;
-		}));
+		});
+		
+		echo json_encode($result);
 	}
     
 	public function validate()
@@ -98,5 +156,4 @@ class pesa extends CI_Controller {
         header('Content-type:Application/json');
 		echo json_encode($this->mpesa->register());
 	}
-    
 }
